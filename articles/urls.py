19acb5067acf2b4f242c8abe2db891a413560b4e
Benedict Owusu <beneyjoy@gmail.com>
2024-03-28 03:36:11 +0000

    fix

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import articles
+from .views import articles, article_detail
 
 app_name = 'articles'
 
 urlpatterns = [
-    path('', articles, name='articles')
+    path('', articles, name='list'),
+    path("<slug:slug>/", article_detail, name='detail')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Article
 
 def articles(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles.html', {'articles': articles})
+
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'details.html', {'article': article})
